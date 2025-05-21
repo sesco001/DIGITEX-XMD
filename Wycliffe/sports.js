c1ee9b1c2c821e7a6efabad8dbba9bb1860b0fb0
Digitexmedia <150277877+Digitexmedia@users.noreply.github.com>
2025-05-21 22:25:55 +0300

    Update sports.js

--- Wycliffe/sports.js
+++ Wycliffe/sports.js
@@ -6,11 +6,11 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio');
-const juma = require(__dirname + "/../config");
+const adams = require(__dirname + "/../config");
 
 async function fetchSportsUrl() {
   try {
-    const response = await axios.get(adams.BWM_XMD);
+    const response = await axios.get(adams.DIGITEX-XMD);
     const $ = cheerio.load(response.data);
 
     const targetElement = $('a:contains("Sports")');
