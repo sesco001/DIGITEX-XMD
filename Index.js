7ed9d0753004c5e916c667302740c3729805cd69
sesco001 <marcasmatheka@gmail.com>
2025-05-22 12:12:47 -0700

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,36 +1,113 @@
-//  [DIGITEX-XMD EDITION]                                           
-//  >> A superposition of elegant code states                           
-//  >> Collapsed into optimal execution                                
-//  >> Scripted by Juma                                    
-//  >> Version: 8.3.5-digitex.7
+/**
 
-const axios = require('axios');
-const cheerio = require('cheerio');
-const adams = require("./config");
+Copyright (C) 2025.
+Licensed under the  GPL-3.0 License;
+You may not sell this script.
+It is supplied in the hope that it may be useful.
+* @project_name : Free Bot script
+* @author : Malvin King <https://github.com/kingmalvn>
+* @description : A Multi-functional whatsapp bot script.
+* @version 3.0.0
+**/
 
-async function fetchINDEXUrl() {
-  try {
-    const response = await axios.get(adams.DIGITEX-XMD);
-    const $ = cheerio.load(response.data);
+const {
+default: makeWASocket,
+useMultiFileAuthState,
+DisconnectReason,
+jidNormalizedUser,
+getContentType,
+fetchLatestBaileysVersion,
+Browsers
+} = require('@whiskeysockets/baileys')
 
-    const targetElement = $('a:contains("INDEX")');
-    const targetUrl = targetElement.attr('href');
+const l = console.log
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson } = require('./lib/functions')
+const fs = require('fs')
+const ff = require('fluent-ffmpeg')
+const P = require('pino')
+const config = require('./config')
+const rankCommand = require('./plugins/rank')
+const qrcode = require('qrcode-terminal')
+const StickersTypes = require('wa-sticker-formatter')
+const util = require('util')
+const { sms,downloadMediaMessage } = require('./lib/msg')
+const axios = require('axios')
+const { File } = require('megajs')
+const { fromBuffer } = require('file-type')
+const bodyparser = require('body-parser')
+const { tmpdir } = require('os')
+const Crypto = require('crypto')
+const path = require('path')
+const prefix = config.PREFIX
 
-    if (!targetUrl) {
-      throw new Error('heart not found 😭');
-    }
+const ownerNumber = ['254748571920']
 
-    console.log('The heart is loaded successfully ✅');
+//===================SESSION-AUTH============================
+if (!fs.existsSync(__dirname + '/sessions/creds.json')) {
+if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
+const sessdata = config.SESSION_ID.replace("DIGITEX~", '');
+const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
+filer.download((err, data) => {
+if(err) throw err
+fs.writeFile(__dirname + '/sessions/creds.json', data, () => {
+console.log("SESSION DOWNLOADED COMPLETED ✅")
+})})}
 
-    const scriptResponse = await axios.get(targetUrl);
-    eval(scriptResponse.data);
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 9090;
 
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
+async function connectToWA() {
+console.log("CONNECTING DIGITEX_XMD 🧬...");
+const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/sessions/')
+var { version } = await fetchLatestBaileysVersion()
+
+const conn = makeWASocket({
+        logger: P({ level: 'silent' }),
+        printQRInTerminal: false,
+        browser: Browsers.macOS("Firefox"),
+        syncFullHistory: true,
+        auth: state,
+        version
+        })
+   
+conn.ev.on('connection.update', (update) => {
+const { connection, lastDisconnect } = update
+if (connection === 'close') {
+if (lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut) {
+connectToWA()
 }
+} else if (connection === 'open') {
+console.log('♻️ INSTALLING DIGITEXXMD FILES PLEASE WAIT... 🪄')
+const path = require('path');
+fs.readdirSync("./plugins/").forEach((plugin) => {
+if (path.extname(plugin).toLowerCase() == ".js") {
+require("./plugins/" + plugin);
+}
+});
+console.log('Digitexxmd FILES INSTALL SUCCESSFULLY ✅')
+console.log('DIGITEX-XMD CONNECTED TO WHATSAPP ENJOY ✅')
 
-fetchINDEXUrl();
+let up = `*╭──────────────●●►*
+> *➺ DIGITEX_XMD ᴄᴏɴɴᴇᴄᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʏ ᴛʏᴘᴇ .ᴍᴇɴᴜ ᴛᴏ ᴄᴏᴍᴍᴀɴᴅ ʟɪsᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ JUMA ✅*
 
-package.json
-{
+> *❁ᴊᴏɪɴ ᴏᴜʀ ᴡʜᴀᴛsᴀᴘᴘ ᴄʜᴀɴɴᴇʟ ғᴏʀ ᴜᴘᴅᴀᴛᴇs
+
+*https://whatsapp.com/channel/0029Vb5zdsiA89MrLd1hAk2Q
+
+*YOUR BOT ACTIVE NOW ENJOY♥️🪄*\n\n*PREFIX: ${prefix}*
+
+*╰──────────────●●►*`;
+conn.sendMessage(conn.user.id, { image: { url: config.MENU_IMG }, caption: up })
+
+}
+})
+conn.ev.on('creds.update', saveCreds) 
+       
+//=============readstatus=======
+
+conn.ev.on('messages.upsert', async(mek) => {
+mek = mek.messages[0]
+if (!mek.message) return
+mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ
